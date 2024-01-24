b389cc79b0b4080c40209154a8ae0871826caa31
Jannah Mekhaemar <j.mekhaemar@gmail.com>
2024-01-24 20:15:38 +0000

    Forgot file

---
+++ Unity/Assets/Code/Scripts/Level/TerrainCellCollection.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+using NobunAtelier;
+
+[CreateAssetMenu(fileName ="DC_TerrainCell", menuName = "NobunAtelier/Collection/TerrainCell")]
+public class TerrainCellCollection : DataCollection<TerrainCellDefinition>
+{
+
+}
