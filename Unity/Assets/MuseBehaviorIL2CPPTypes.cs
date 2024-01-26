d5f4e2c9d5f66d7dfa3ae2bf760757468fe147d6
Morgan Hoarau <43406539+Nobuna-no@users.noreply.github.com>
2024-01-26 14:54:16 +0000

    Improves player physics and minor other fixes
    Also build for il2cpp

---
+++ Unity/Assets/MuseBehaviorIL2CPPTypes.cs
@@ -0,0 +1,20 @@
+using Graph;
+
+namespace Unity.Muse.Behavior
+{
+	sealed class MuseBehaviorTypes
+	{
+		private MuseBehaviorTypes() { }
+
+		TypedVariableModel<UnityEngine.GameObject> m_TypedVariableModelUnityEngineGameObject;
+		TypedVariableModel<System.String> m_TypedVariableModelSystemString;
+		TypedVariableModel<System.Int32> m_TypedVariableModelSystemInt32;
+		TypedVariableModel<System.Single> m_TypedVariableModelSystemSingle;
+		TypedVariableModel<System.Double> m_TypedVariableModelSystemDouble;
+		TypedVariableModel<System.Boolean> m_TypedVariableModelSystemBoolean;
+		TypedVariableModel<UnityEngine.Vector2> m_TypedVariableModelUnityEngineVector2;
+		TypedVariableModel<UnityEngine.Vector3> m_TypedVariableModelUnityEngineVector3;
+		TypedVariableModel<UnityEngine.Color> m_TypedVariableModelUnityEngineColor;
+	}
+}
+
